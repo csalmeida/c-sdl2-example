c3d84e73091c80a6102b3cd6be08af57a222627a
Cristiano <cs.almeida@protonmail.ch>
2024-07-06 12:52:18 +0100

    Instantiates SDL in main example.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]) 
 {
-  printf("Hello, SDL\n");
+
+  // Initializes SDL with selected subsystems:
+  int sdl = SDL_Init(SDL_INIT_VIDEO);
+
+  // Exits if SDL was not initialized correctly:
+  bool sdl_is_init = sdl == 0;
+  if (!sdl_is_init) {
+    fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  printf("🎉 Hello, SDL 🎉\n");
   return 0;
 }
